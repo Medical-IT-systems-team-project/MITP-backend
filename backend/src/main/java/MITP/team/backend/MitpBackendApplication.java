3dd0bdcb0139effe566e744630208728fe530233
kamilgolawski <kamilgolawski3@icloud.com>
2024-10-27 23:38:41 +0100

    Project setup + gitignore

---
+++ backend/src/main/java/MITP/team/backend/MitpBackendApplication.java
@@ -0,0 +1,13 @@
+package MITP.team.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitpBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/MITP/team/backend/MitpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package MITP.team.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
