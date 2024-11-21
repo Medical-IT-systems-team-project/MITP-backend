d68a7372dc4731a0279cfa44f4ed60814f75922e
kamilgolawski <kamilgolawski3@icloud.com>
2024-11-21 11:06:12 +0100

    bugfix - patient validation

--- backend/src/main/java/MITP/team/backend/Controller/PatientController.java
+++ backend/src/main/java/MITP/team/backend/Controller/PatientController.java
@@ -2,8 +2,10 @@ package MITP.team.backend.Controller;
 
 import MITP.team.backend.Model.Dto.PatientRequestDto;
 import MITP.team.backend.Model.Dto.PatientResponseDto;
+import MITP.team.backend.Model.Patient;
 import MITP.team.backend.Service.PatientService;
 import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -11,18 +13,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/patient")
 public class PatientController {
 
-    private final PatientService patientService;
+  private final PatientService patientService;
 
-    @PostMapping("/new")
-    public PatientResponseDto createNewPatient(@RequestBody PatientRequestDto patientRequestDto) {
-        String accessId = patientService.createNewPatient(patientRequestDto);
-        return PatientResponseDto.builder()
-                .accessId(accessId)
-                .build();
-    }
+  @PostMapping("/new")
+  public PatientResponseDto createNewPatient(
+      @Validated(Patient.CreateValidation.class) @RequestBody PatientRequestDto patientRequestDto) {
+    String accessId = patientService.createNewPatient(patientRequestDto);
+    return PatientResponseDto.builder().accessId(accessId).build();
+  }
 
-    @GetMapping("/{accessId}")
-    public PatientResponseDto getPatientByAccessId(@PathVariable String accessId) {
-        return patientService.getPatientByAccessId(accessId);
-    }
+  @GetMapping("/{accessId}")
+  public PatientResponseDto getPatientByAccessId(@PathVariable String accessId) {
+    return patientService.getPatientByAccessId(accessId);
+  }
 }
--- backend/src/main/java/MITP/team/backend/Model/Patient.java
+++ backend/src/main/java/MITP/team/backend/Model/Patient.java
@@ -1,33 +1,44 @@
 package MITP.team.backend.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.*;
 
 @Builder
 @Getter
-@NoArgsConstructor
 @Entity
 @Table(name = "patient")
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor
 public class Patient {
+  @NotBlank(groups = {CreateValidation.class, UpdateValidation.class})
+  private String firstName;
+  @NotBlank(groups = {CreateValidation.class, UpdateValidation.class})
+  private String lastName;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "patient_id", nullable = false)
   private Long id;
-
-  private String firstName;
-  private String lastName;
-  private int age;
-
+  @NotNull(groups = UpdateValidation.class)
   @Column(name = "birth_date")
   private LocalDateTime birthDate;
+  @NotNull(groups = {CreateValidation.class})
+  @Column(nullable = false)
+  private Long socialSecurityNumber;
+
+  private int age;
+
+  public interface CreateValidation {}
 
   private String gender;
   private String accessId;
-  private Long socialSecurityNumber;
+
+  public interface UpdateValidation {}
+
   private String email; // TODO validate REGEX
   private String phoneNumber; // TODO validate REGEX
   private String address;
