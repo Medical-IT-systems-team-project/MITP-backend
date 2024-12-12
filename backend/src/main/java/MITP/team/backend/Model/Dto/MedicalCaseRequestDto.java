0fddcca716f3f14f524f300cd93b7b5fe15e7354
Krystian <wasilkrystian78@gmail.com>
2024-12-12 08:45:53 +0100

    poprawa bledu w dto PatientResponse

--- backend/src/main/java/MITP/team/backend/Model/Dto/MedicalCaseRequestDto.java
+++ backend/src/main/java/MITP/team/backend/Model/Dto/MedicalCaseRequestDto.java
@@ -2,11 +2,13 @@ package MITP.team.backend.Model.Dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
 import java.time.LocalDateTime;
 
 public record MedicalCaseRequestDto(
     @NotNull Long patientId,
     @NotNull String admissionReason,
-    @NotNull LocalDateTime admissionDate,
+    @NotNull @PastOrPresent LocalDateTime admissionDate,
     @NotBlank String description,
     @NotNull Long attendingDoctorId) {}
--- backend/src/main/java/MITP/team/backend/Model/Dto/MedicationRequestDto.java
+++ backend/src/main/java/MITP/team/backend/Model/Dto/MedicationRequestDto.java
@@ -2,12 +2,15 @@ package MITP.team.backend.Model.Dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Builder;
 
 @Builder
 public record MedicationRequestDto(
         String name,
+        @PastOrPresent
         LocalDateTime startDate,
+        @PastOrPresent
         LocalDateTime endDate,
         String details,
         Long medicalCaseId,
